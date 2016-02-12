8a6513546cffd2469683ea2d9e3b30ac143d51f6
GabbaGandalf <jm.wanka@gmail.com>
2016-02-12 13:30:40 +0100

    teleport

--- init.lua
+++ init.lua
@@ -1,72 +1,50 @@
+function tp(c, sector)
+	local delta = 20
+	local s = client.focus.screen 
+	local screengeom = screen[s].workarea
 
-function tileTo(c, sector)
-   
-   local s = client.focus.screen 
-   local screengeom = screen[s].workarea
+	local awful     = require("awful")
+	local geometrySet = {}
+	local curgeo =	c:geometry() 
 
-   local geometrySet = {}
+	geometrySet['left-top'] = {
+		x = screengeom.x + delta,
+		y = screengeom.y + delta,
+	}
+	geometrySet['right-top'] = {
+		x = screengeom.x + screengeom.width-curgeo.width-2*c.border_width - delta,
+		y = screengeom.y + delta,
+	}
 
-   geometrySet['left'] = {
-      x = screengeom.x,
-      y = screengeom.y,
-      width = screengeom.width / 2,
-      height = screengeom.height
-   }
-   geometrySet['right'] = {
-      x = screengeom.width / 2,
-      y = screengeom.y,
-      width = screengeom.width / 2,
-      height = screengeom.height
-   }
-   
-   geometrySet['left-top'] = {
-      x = screengeom.x,
-      y = screengeom.y,
-      width = screengeom.width / 2,
-      height = screengeom.height / 2
-   }
-   geometrySet['left-bottom'] = {
-      x = screengeom.x,
-      y = screengeom.height / 2,
-      width = screengeom.width / 2,
-      height = screengeom.height / 2
-   }
-   geometrySet['right-top'] = {
-      x = screengeom.width / 2,
-      y = screengeom.y,
-      width = screengeom.width / 2,
-      height = screengeom.height /2
-   }
-   geometrySet['right-bottom'] = {
-      x = screengeom.width / 2,
-      y = screengeom.height / 2 ,
-      width = screengeom.width / 2,
-      height = screengeom.height / 2
-   }
-   geometrySet['top'] = {
-      x = screengeom.x,
-      y = screengeom.y,
-      width = screengeom.width, 
-      height = screengeom.height / 2
-   }
-   geometrySet['bottom'] = {
-      x = screengeom.x,
-      y =  screengeom.height / 2,
-      width = screengeom.width,
-      height = screengeom.height / 2
-   }
-   geometrySet['full'] = {
-      x = screengeom.x,
-      y =  screengeom.y,
-      width = screengeom.width,
-      height = screengeom.height
-   }
-   
-   c:geometry(geometrySet[sector])
-   
+	geometrySet['left-bottom'] = {
+		x = screengeom.x + delta,
+		y = screengeom.y + screengeom.height - curgeo.height - 2*c.border_width - delta,
+	}
+	geometrySet['right-bottom'] = {
+		x = screengeom.x + screengeom.width - curgeo.width - 2*c.border_width - delta,
+		y = screengeom.y + screengeom.height  - curgeo.height - 2*c.border_width - delta,
+	}
+	geometrySet['middle'] = {
+		x = screengeom.x + screengeom.width/2 - curgeo.width/2 - c.border_width ,
+		y = screengeom.y + screengeom.height/2 -curgeo.height/2 - c.border_width ,
+	}
+	geometrySet['left-middle'] = {
+		x = screengeom.x + delta,
+		y = screengeom.y + screengeom.height/2 -curgeo.height/2 - c.border_width ,
+	}
+	geometrySet['right-middle'] = {
+		x = screengeom.x +screengeom.width - curgeo.width - 2*c.border_width - delta, 
+		y = screengeom.y + screengeom.height/2 -curgeo.height/2 - c.border_width ,
+	}
+
+
+	c:geometry(geometrySet[sector])
+
 end
 
 local handTiler = {}
-handTiler.tileTo = tileTo
+handTiler.tp = tp
+
+
 
 return  handTiler
